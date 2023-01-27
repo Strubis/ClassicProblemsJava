0c2d5d14bc3ec4e5acec5d628c8c59feeee5365d
Emerson Lima <68118899+Strubis@users.noreply.github.com>
2023-01-26 23:52:29 -0300

    Implementacao Busca em Profundidade

--- src/chapter02/GenericSearch.java
+++ src/chapter02/GenericSearch.java
@@ -1,6 +1,12 @@
 package chapter02;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Algoritmos de busca generica, utilizando o objeto generico T
@@ -21,6 +27,84 @@ public class GenericSearch {
 		}
 		
 		return false;
+	}
+	
+	// Classe Node que sera utilizada para melhorar os algoritmos de busca
+	static class Node<T> implements Comparable<Node<T>>{
+		final T state;
+		
+		// Caso um no nao tenha pai, utilizaremos um sentinela para indicar isso
+		Node<T> parent;
+		double cost, heuristic;
+		
+		// Para os algoritmos DFS e BFS nao usamos custo e heuristica
+		Node(T state, Node<T> parent){
+			this.state = state;
+			this.parent = parent;
+		}
+		
+		// Para o algoritmo de A*
+		Node(T state, Node<T> parent, double cost, double heuristic){
+			this.state = state;
+			this.parent = parent;
+			this.cost = cost;
+			this.heuristic = heuristic;
+		}
+
+		@Override
+		public int compareTo(Node<T> o) {
+			Double mine = cost + heuristic;
+			Double theirs = o.cost + o.heuristic;
+			return mine.compareTo(theirs);
+		}
+	}
+	
+	// Busca em Profundidade -> DFS
+	static <T> Node<T> dfs(T initial, Predicate<T> goalTest, Function<T, List<T>> successors){
+		// frontier sao lugares nao visitados ainda
+		Stack<Node<T>> frontier = new Stack<>();
+		frontier.push(new Node<>(initial, null));
+		
+		// explored sao lugares ja visitados
+		Set<T> explored = new HashSet();
+		explored.add(initial);
+		
+		// continua procurando enquanto ha lugar
+		while(!frontier.isEmpty()) {
+			Node<T> currentNode = frontier.pop();
+			T currentState = currentNode.state;
+		
+			// Se encontrarmos o objetivo, entao retornamos ele
+			if(goalTest.test(currentState)) {
+				return currentNode;
+			}
+			
+			// Checa para onde podemos ir
+			for(T child : successors.apply(currentState)) {
+				if(explored.contains(child)) {
+					continue; // pula o filho ja explorado
+				}
+				
+				explored.add(child);
+				frontier.push(new Node<>(child, currentNode));
+			}
+		}
+		
+		return null; // Procuramos e nao foi encontrado
+	}
+	
+	// Caminho do no
+	static <T> List<T> nodeToPath(Node<T> node){
+		List<T> path = new ArrayList<>();
+		path.add(node.state);
+		
+		// Faz o caminho do fim para o inicio
+		while(node.parent != null) {
+			node = node.parent;
+			path.add(0, node.state); // add para o inicio
+		}
+		
+		return path;
 	}
 	
 	// Busca Binaria, assumindo que a lista esta ordenada
